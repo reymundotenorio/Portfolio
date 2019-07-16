46ea2be6c3cc07beec362101191b587ef4acbbe2
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-07-16 15:22:40 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.eb52d3f27ca3ad2593d0fcbe17f0353c.js"
+  "/portfolio/precache-manifest.55e94ccce4cbf1848e4c465f1ed47b09.js"
 );
 
 self.addEventListener('message', (event) => {
