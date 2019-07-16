21760a51b734d90ecb6576ecd0e8ac360cf7669b
Reymundo Tenorio <reymundotenorio@gmail.com>
2019-07-16 15:11:14 -0700

    Adding the Intro component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import develop from "./img/develop.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="intro">
+      <div className="container">
+        <div className="row flex-container">
+          <div className="col-xs-12 col-sm-6">
+            <h1>
+              <span>I'm</span> <span>Reymundo</span> <span>Tenorio</span>
+            </h1>
+            <h2>Software Developer</h2>
+          </div>
+
+          <div className="col-xs-12 col-sm-6">
+            <div className="image-container">
+              <img src={develop} className="img-responsive" alt="Develop" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
 
